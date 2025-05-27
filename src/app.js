ce9a81acfcfe8342b7225c5c47fbee7b8bca2deb
Teja <saitejagunjala@gmail.com>
2025-05-26 21:48:53 -0500

    test1 del

--- src/app.js
+++ src/app.js
@@ -8,29 +8,20 @@ const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./middlewares/auth");
-const cors = require("cors")
-app.use(cors({credentials: true, origin: "http://localhost:5173"}))
+const cors = require("cors");
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/requests"); 
-const {userRouter} = require("./routes/user");
-
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
+const requestRouter = require("./routes/requests");
+const { userRouter } = require("./routes/user");
 
 app.use("/", authRouter);
 app.use("/", userAuth, profileRouter); //If `userAuth` is not applied globally, there's a risk of forgetting to secure a route, leaving it exposed to unauthorized access. Applying it at the `app.use()` level ensures consistent security across all protected routes, reducing the chance of human error and vulnerabilities.
-app.use("/", userAuth, requestRouter); 
-app.use("/",userRouter); 
-
-
-
-
-
+app.use("/", userAuth, requestRouter);
+app.use("/", userRouter);
 
 const port = 7777; //becoz  port should not change once it's set.
 
